2527f4f78ba0196f74b0da7459c59ac5984f8b84
sandrofarizel@hotmail.com <sandrofarizel@hotmail.com>
2021-04-15 21:01:34 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -11,9 +11,9 @@ namespace SalesWebMvc.Services
     {
         private readonly SalesWebMvcContext _context;
 
-        public SellerService (SalesWebMvcContext context )
+        public SellerService(SalesWebMvcContext context)
         {
-            _context = context; 
+            _context = context;
         }
 
         public List<Seller> FindAll()
@@ -27,5 +27,18 @@ namespace SalesWebMvc.Services
             _context.SaveChanges();
 
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges(); 
+        }
+
     }
 }
